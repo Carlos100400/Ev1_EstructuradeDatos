b6f856b98066d15e4497df2a13a1c4301535fb9a
GeraIAA <133793956+GeraIAA@users.noreply.github.com>
2023-09-04 20:02:23 -0600

    Evidencia1.py

--- Evidencia1.py
+++ Evidencia1.py
@@ -81,6 +81,43 @@ match opcion:
     case 3:
         print("caso3")
     case 4:
-        print("caso4")
+         print("Recuperar una nota:")
+            print("Folios de notas canceladas:")
+
+
+            tabla_folios_cancelados = PrettyTable()
+            tabla_folios_cancelados.field_names = ["Folio"]
+            for folio_cancelada in not_cancel.keys():
+                tabla_folios_cancelados.add_row([folio_cancelada])
+
+            print(tabla_folios_cancelados)
+
+            consulta_folio = int(input("Ingrese el folio de la nota que desea recuperar (0 para cancelar): "))
+
+            if consulta_folio == 0:
+                print("No se recuperó ninguna nota.")
+            elif consulta_folio in not_cancel:
+                datos_guardados = not_cancel[consulta_folio]
+
+                tabla_detalle_recuperada = PrettyTable()
+                tabla_detalle_recuperada.field_names = ["Referencia", "Datos"]
+                tabla_detalle_recuperada.add_row(["Folio", datos_guardados[0]])
+                tabla_detalle_recuperada.add_row(["Fecha", datos_guardados[1]])
+                tabla_detalle_recuperada.add_row(["Cliente", datos_guardados[2]])
+                tabla_detalle_recuperada.add_row(["Servicios", datos_guardados[3]])
+                tabla_detalle_recuperada.add_row(["Monto", datos_guardados[4]])
+
+                print("Detalle de la nota a recuperar:")
+                print(tabla_detalle_recuperada)
+
+                confirmacion = input("¿Está seguro de que desea recuperar esta nota? (Si/No): ")
+                if confirmacion.lower() == "si":
+                    not_guardada[consulta_folio] = datos_guardados
+                    del not_cancel[consulta_folio]
+                    print("Nota recuperada exitosamente.")
+                else:
+                    print("Cancelación de recuperación de nota.")
+            else:
+                print("El folio ingresado no corresponde a una nota cancelada.")
     case 5:
         print("Gracias por su preferencia. Vuelva pronto.")

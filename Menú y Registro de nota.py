979c2c511c86939e36bb4f8f82799a7ae502751d
Myriam Avila <143561013+myriav@users.noreply.github.com>
2023-09-24 23:30:45 -0600

    Add files via upload

---
+++ Menú y Registro de nota.py
@@ -0,0 +1,163 @@
+from prettytable import PrettyTable
+import datetime
+import re
+import csv
+import openpyxl
+
+def exportar_a_excel(cliente, notas):
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = f"Notas_{cliente}_{datetime.date.today()}"
+
+    ws.append(["Folio", "Fecha", "Cliente", "RFC", "Correo", "Servicio", "Total a Pagar"])
+
+    for folio, fecha, _, rfc, correo, servicios, total_costo in notas:
+        ws.append([folio, fecha, cliente, rfc, correo, "\n".join([f"{servicio}: ${costo:.2f}" for servicio, costo in servicios]), f"${total_costo:.2f}"])
+
+    excel_filename = f"{cliente}_{datetime.date.today()}.xlsx"
+    wb.save(excel_filename)
+    print(f"La información se ha exportado a {excel_filename}")
+
+folios = {}
+not_guardada = {}
+not_cancel = {}
+respuesta2 = ""
+regreso = ""
+
+while respuesta2 == '':
+    print('''
+      *** Menú Principal ***
+      1 - Registro de nota
+      2 - Consultas y reportes
+      3 - Cancelar una nota
+      4 - Recuperar una nota
+      5 - Salir del menú
+      ''')
+
+    while True:
+        try:
+            opcion = int(input("\nSeleccione la opción que desee (Unicamente números): "))
+        except ValueError:
+            print("Solamente se aceptan valores numéricos. Inténtelo de nuevo.")
+        except Exception:
+            print("Opción no válida. Inténtelo de nuevo.")
+        else:
+            if opcion < 6:
+                break
+            else:
+                print("Solamente se aceptan valores del 1 al 5. Inténtelo de nuevo.")
+
+    match opcion:
+        case 1:
+            folio = max(folios, default=100) + 1
+            folios[folio] = ''
+
+            while True:
+                cliente = input('Nombre del cliente: ')
+                if cliente.strip():
+                    break
+                else:
+                    print("Inténtelo de nuevo.")
+
+            while True:
+                rfc = input('Ingrese su RFC: ')
+
+                if rfc == "":
+                    print("El RFC no puede omitirse, inténtelo de nuevo.")
+                    continue
+
+                if not re.match('^[A-Z]{4}\d{6}[A-Z0-9]{3}$', rfc):
+                    print("RFC no válido. Inténtelo de nuevo.")
+                    continue
+
+                break
+
+            while True:
+                correo = input("Ingrese su correo electrónico: ")
+
+                if correo == "":
+                    print("El Correo no puede omitirse, inténtelo de nuevo.")
+                    continue
+
+                if not re.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', correo):
+                    print("Correo electrónico no válido.")
+                    continue
+
+                break
+
+            servicios = []
+            total_costo = 0.0
+
+            while True:
+                servicio = input("Ingrese un servicio (o deje un espacio en blanco para terminar): ")
+                if servicio.strip() == "":
+                    break
+                while True:
+                    try:
+                        costo = float(input("Ingrese el costo del servicio: "))
+                    except ValueError:
+                        print('Solamente acepta valores numéricos. Inténtelo de nuevo.')
+                    except Exception:
+                        print("Dato no válido. Inténtelo de nuevo.")
+                    else:
+                        if costo > 0:
+                            servicios.append((servicio, costo))
+                            total_costo += costo
+                            break
+                        else:
+                            print('El precio del servicio tiene que ser mayor a 0.')
+
+            fecha_act = datetime.date.today()
+            while True:
+                try:
+                    while True:
+                        fecha_proporcionada = input('Fecha de la nota (dd/mm/aaaa): ')
+                        fecha_ing = datetime.datetime.strptime(fecha_proporcionada, "%d/%m/%Y").date()
+                        fecha_cliente = fecha_ing.strftime("%d/%m/%Y")
+                        if fecha_ing <= fecha_act:
+                            break
+                        else:
+                            print('La fecha no puede ser posterior a la actual del sistema\n')
+                except ValueError:
+                    print('Tipo de formato no válido. Inténtelo de nuevo\n')
+                except Exception as error:
+                    print(f'Ocurrió un problema: {error}\n')
+                else:
+                    break
+
+            datos_guardados = [folio, fecha_cliente, cliente, rfc, correo, servicios, total_costo]
+
+            tabla = PrettyTable()
+            tabla.field_names = ['Folio', 'Nombre', 'RFC', 'Correo', 'Fecha', 'Servicio', 'Total a Pagar']
+            tabla.add_row([f'{folio}', f'{cliente}', f'{rfc}', f'{correo}', f'{fecha_cliente}',
+                           '\n'.join([f"{servicio}: ${costo:.2f}" for servicio, costo in servicios]),
+                           f'${total_costo:.2f}'])
+            print(tabla)
+
+            while True:
+                respuesta = int(input(' Seleccione la opción que desee (1 - Guardar o 2 - Cancelar): '))
+                if respuesta == 1:
+                    print('*** Nota guardada ***')
+
+                    not_guardada[folio] = datos_guardados
+
+                    break
+                elif respuesta == 2:
+                    print('*** Nota cancelada ***')
+                    not_cancel[folio] = datos_guardados
+                    break
+                else:
+                    print('No se encuentra esa opción. Inténtelo de nuevo.')
+        case 2:
+            pass
+        case 3:
+            pass
+
+        case 4:
+            pass
+
+        case 5:
+            pass
+        case _:
+            print("Seleccione una opción del menú")
+    respuesta2 = ""
